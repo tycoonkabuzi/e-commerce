4ec4233f2096e3a81c4a9161ae6caf1acff81a82
Tycoon <tycoon@Tycoons-MacBook-Pro.local>
2025-10-14 15:41:38 +0200

    nav done visually

--- src/components/layouts/nav/Nav.tsx
+++ src/components/layouts/nav/Nav.tsx
@@ -1,5 +1,39 @@
+import { Icon } from "@iconify/react";
+import logo from "../../../assets/icons/toni-black-full.png";
+import style from "../nav/Nav.module.scss";
 const Nav: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className={style.nav}>
+      <div className={style.logo}>
+        <img src={logo} />
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>Shop</li>
+        <li>Blog</li>
+      </ul>
+
+      <ul>
+        <li>
+          <Icon icon="line-md:search" width={25} height={25} />
+          Search
+        </li>
+        <li>
+          <Icon icon="line-md:account" width={25} height={25} />
+        </li>
+        <li>
+          <Icon icon="line-md:heart" width={25} height={25} />
+        </li>
+        <li>
+          <Icon
+            icon="material-symbols:shopping-cart-outline"
+            width={25}
+            height={25}
+          />
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Nav;
