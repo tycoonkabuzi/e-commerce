8b1d6a4c811123f0edd0da65a966eb86c96b14bb
Tycoon Kabuzi <kabuzitycoon@gmail.com>
2025-10-15 16:34:35 +0200

    shop category almost done only adjusting the images

--- src/components/layouts/categories/Category.tsx
+++ src/components/layouts/categories/Category.tsx
@@ -1,14 +1,47 @@
 import styles from "./Category.module.scss";
 
+import { Icon } from "@iconify/react";
+import computer from "../../../assets/images/computer.png";
+import toys from "../../../assets/images/toys.png";
+import beauty from "../../../assets/images/beauty.png";
 const Category: React.FC = () => {
   return (
     <div className={styles.categories}>
-      <h3> Shop by categories</h3>
+      <div className={styles.titles}>
+        <h3> Shop by categories</h3>
+        <h3 style={{ display: "flex" }}>
+          All departments
+          <Icon icon="mdi-light:arrow-right" width={25} height={25} />{" "}
+        </h3>
+      </div>
 
-      <div>
-        <div>
-          <img src="" alt="Lotion" />
-          <p>Beauty picks</p>
+      <div className={styles.items_hero}>
+        <div className={styles.item}>
+          <div className={styles.container_image}>
+            <img src={computer} alt="Macbook computer" />
+          </div>
+
+          <h3>Computer & Accessories</h3>
+        </div>
+
+        <div className={styles.item}>
+          <div className={styles.container_image}>
+            <img src={toys} alt="teddy ber" />
+          </div>
+          <h3>Toys & Ganes</h3>
+        </div>
+
+        <div className={styles.item}>
+          <div className={styles.container_image}>
+            <img src={computer} alt="Lotion" />
+          </div>
+          <h3>Fashion</h3>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.container_image}>
+            <img src={beauty} alt="black woman with a leaf" />
+          </div>
+          <h3>Beauty picks</h3>
         </div>
       </div>
     </div>
