6b9c2ba5fee1a3e94150c6263354f7b3358de825
Tycoon <tycoon@Tycoons-MacBook-Pro.local>
2025-10-16 20:14:04 +0200

    made the menu reusable and trying to make the card Text reusable

---
+++ src/components/layouts/menu/Menu.tsx
@@ -0,0 +1,21 @@
+import { Icon } from "@iconify/react";
+import styles from "./Menu.module.scss";
+
+const Menu = ({
+  bigTitle,
+  smallTitle,
+}: {
+  bigTitle: string;
+  smallTitle: string;
+}) => {
+  return (
+    <div className={styles.titles}>
+      <h3> {bigTitle}</h3>
+      <h3 style={{ display: "flex" }}>
+        {smallTitle}
+        <Icon icon="mdi-light:arrow-right" width={25} height={25} />{" "}
+      </h3>
+    </div>
+  );
+};
+export default Menu;
