2b6010fc7eb82fe252c32e1a2c404cb86a8cdffb
Tycoon Kabuzi <kabuzitycoon@gmail.com>
2025-10-17 15:58:35 +0200

    refactored the Delivery folder to a reusable one it is called reusableBanner

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
-import BannerDelivery from "./components/layouts/bannerDelivery";
+import ReusableBanner from "./components/layouts/reusableBanner";
 import BannerSales from "./components/layouts/bannerSales";
 import Category from "./components/layouts/categories";
 import Header from "./components/layouts/header";
 import LastViewed from "./components/layouts/lastViewed";
 import Nav from "./components/layouts/nav";
 import PersonalOffer from "./components/layouts/personalOffer";
-
+import delivery from "./assets/images/delivery.png";
+import woman from "./assets/images/woman.png";
 function App() {
   return (
     <>
@@ -15,8 +16,27 @@ function App() {
       <PersonalOffer />
       <Category />
       <BannerSales />
-      <BannerDelivery />
+      <ReusableBanner
+        subtitle={"Discover Toni"}
+        title={"TONI DELIVERS TO YOU"}
+        content={
+          "Worldwide shipping. We ship to over 100 countries and regions, right to your doorstep."
+        }
+        button={"View more"}
+        image={{ src: delivery, alt: "A delivery guy on a bike " }}
+      />
+
       <LastViewed />
+
+      <ReusableBanner
+        subtitle={"Discover Toni"}
+        title={"SUBSCRIBE TO THE NEWS"}
+        content={
+          "Worldwide shipping. We ship to over 100 countries and regions, right to your doorstep."
+        }
+        button={"Subscribe"}
+        image={{ src: woman, alt: "A woman smilling wearing orange" }}
+      />
     </>
   );
 }
