702088b83706ca672e861ce2ae14389be84d07b3
Tycoon <tycoon@Tycoons-MacBook-Pro.local>
2025-10-20 16:19:15 +0200

    added a new div to wrap all the headers for the carrouisel effect.

--- src/components/layouts/header/Header.tsx
+++ src/components/layouts/header/Header.tsx
@@ -3,45 +3,93 @@ import headset from "../../../assets/images/blue-headsets.png";
 import CardText from "../cardText";
 
 const Header: React.FC = () => {
+
+  const moveHeader(): =>{
+
+  }
   return (
-    <div className={styles.header}>
-      <div>
-        <div
-          className={`${styles.main_button_selection} ${styles.selected_item} `}
-        >
-          <span />
-        </div>
+    <div className={styles.header_hero}>
+      <div className={styles.header}>
+        <div>
+          <div
+            className={`${styles.main_button_selection} ${styles.selected_item} `}
+          >
+            <span />
+          </div>
 
-        <div className={`${styles.main_button_selection} `}>
-          <span />
-        </div>
+          <div className={`${styles.main_button_selection} `}>
+            <span />
+          </div>
 
-        <div className={`${styles.main_button_selection} `}>
-          <span />
-        </div>
+          <div className={`${styles.main_button_selection} `}>
+            <span />
+          </div>
 
-        <div className={`${styles.main_button_selection} `}>
-          <span />
+          <div className={`${styles.main_button_selection} `}>
+            <span />
+          </div>
+        </div>
+        <div className={styles.shop_hero}>
+          <h1>SHOP COMPUTERS & ACCESSORIES 1</h1>
+          <p>
+            Shop laptops. desktops. monitors. tablets. PC gaming. hard drives
+            and storage, accessories and more
+          </p>
+          <button className={styles.btn}>View more</button>
+        </div>
+        <div className={styles.sample_product}>
+          <CardText
+            subtitle={"Computer & accessories"}
+            title={"JBL T460BT Black Headphones"}
+            price={"$124.00"}
+            previousPrice={"$250.00"}
+            button={"View more"}
+          />
+          <div className={styles.image}>
+            <img src={headset} alt="headsets" />
+          </div>
         </div>
       </div>
-      <div className={styles.shop_hero}>
-        <h1>SHOP COMPUTERS & ACCESSORIES</h1>
-        <p>
-          Shop laptops. desktops. monitors. tablets. PC gaming. hard drives and
-          storage, accessories and more
-        </p>
-        <button className={styles.btn}>View more</button>
-      </div>
-      <div className={styles.sample_product}>
-        <CardText
-          subtitle={"Computer & accessories"}
-          title={"JBL T460BT Black Headphones"}
-          price={"$124.00"}
-          previousPrice={"$250.00"}
-          button={"View more"}
-        />
-        <div className={styles.image}>
-          <img src={headset} alt="headsets" />
+
+      <div className={styles.header}>
+        <div>
+          <div className={`${styles.main_button_selection} `}>
+            <span />
+          </div>
+
+          <div
+            className={`${styles.main_button_selection} ${styles.selected_item} `}
+          >
+            <span />
+          </div>
+
+          <div className={`${styles.main_button_selection} `}>
+            <span />
+          </div>
+
+          <div className={`${styles.main_button_selection} `}>
+            <span />
+          </div>
+        </div>
+        <div className={styles.shop_hero}>
+          <h1>SHOP COMPUTERS & ACCESSORIES 2</h1>
+          <p>
+            Shop laptops. desktops. monitors. tablets. PC gaming. hard drives
+            and storage, accessories and more
+          </p>
+          <button className={styles.btn}>View more</button>
+        </div>
+        <div className={styles.sample_product}>
+          <CardText
+            subtitle={"Computer & accessories"}
+            title={"JBL T460BT Black Headphones"}
+            price={"$124.00"}
+            previousPrice={"$250.00"}
+            button={"View more"}
+          />
+          <div className={styles.image}>
+            <img src={headset} alt="headsets" />
+          </div>
         </div>
       </div>
     </div>
