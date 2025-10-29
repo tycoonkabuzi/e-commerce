367cc12e4962e3aa3097d1efdf976b2a8cde4043
Tycoon <tycoon@Tycoons-MacBook-Pro.local>
2025-10-29 12:36:33 +0100

    done with the newsletter component

--- src/components/layouts/newsletter/NewsLetter.tsx
+++ src/components/layouts/newsletter/NewsLetter.tsx
@@ -1,5 +1,33 @@
+import styles from "./NewsLetter.module.scss";
+import { Icon } from "@iconify/react";
+
 const NewsLetter: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className={styles.newsLetter}>
+      <h3 className={styles.big_title}>
+        Subscribe to our news letter to get updates of new products
+      </h3>
+      <p>
+        Get 20% off on your first order just by subscribing to our news letter{" "}
+      </p>
+      <form action="" className={styles.form}>
+        <div className={styles.input_wrapper}>
+          <Icon
+            className={styles.search_icon}
+            icon="ri:mail-line"
+            width={20}
+            height={20}
+          />
+          <input type="text" placeholder="Subscribe to our news letter " />
+          <button className={styles.btn_primary}>Subscribe</button>
+        </div>
+      </form>
+      <p className={`${styles.subscribe_text}  ${styles.small_paragraph}`}>
+        You will be able to unsubscribe at any time read our privacy policy
+        <a href=""> here</a>
+      </p>
+    </div>
+  );
 };
 
 export default NewsLetter;
