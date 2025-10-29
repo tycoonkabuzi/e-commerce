080d6b8654a26152baa87b531c1312fbcef4a062
Tycoon Kabuzi <kabuzitycoon@gmail.com>
2025-10-29 11:48:01 +0100

    Initialised the Newsletter folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import "./App.css";
-import Card from "./components/layouts/card";
-import FilterSection from "./components/layouts/filterSection";
-import HeaderWithSearch from "./components/layouts/headerWithSearch";
-import Nav from "./components/layouts/nav";
+
+import Shop from "./components/pages/Shop";
 
 function App() {
   return (
     <>
-      {/* <Home /> */}
-      <Nav />
-      <HeaderWithSearch />
-
-      <div>
-        <FilterSection />
-        <div>
-          <Card /> <Card /> <Card />
-        </div>
-      </div>
+      <Shop />
     </>
   );
 }
---
+++ src/components/layouts/newsletter/NewsLetter.tsx
@@ -0,0 +1,5 @@
+const NewsLetter: React.FC = () => {
+  return <div></div>;
+};
+
+export default NewsLetter;
