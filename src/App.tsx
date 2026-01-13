fe7787d0a9a37c1b38772cd0c2dba31fef5c17d1
Tycoon <kabuzitycoon@gmail.com>
2026-01-13 15:44:22 +0100

    Initialising data for the exercice

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import "./App.css";
+import "./styles/main.scss";
 import Home from "./components/pages/Home";
 import Shop from "./components/pages/Shop";
+import ExerciceComponent from "./components/pages/ExerciceComponent";
 
 function App() {
   return (
     <>
-      <Home />
+      <ExerciceComponent />
     </>
   );
 }
---
+++ src/components/pages/ExerciceComponent.tsx
@@ -0,0 +1,20 @@
+const ExerciceComponent: React.FC = () => {
+  const mockData = {
+    london: {
+      temperature: "18deg",
+      wind: "20km/h",
+    },
+    southAfrica: {
+      temperature: "28deg",
+      wind: "0km/h",
+    },
+
+    "New york": {
+      temperature: "28deg",
+      wind: "0km/h",
+    },
+  };
+  return <div></div>;
+};
+
+export default ExerciceComponent;
